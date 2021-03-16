6c60374f2f0205b72a19d1c13bb331e500645e4c
RafaelKleinH <79064265+RafaelKleinH@users.noreply.github.com>
2021-03-16 18:20:47 -0300

    Commit

--- Carangas/AddEditViewController.swift
+++ Carangas/AddEditViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class AddEditViewController: UIViewController {
-
+    
     // MARK: - IBOutlets
     @IBOutlet weak var tfBrand: UITextField!
     @IBOutlet weak var tfName: UITextField!
@@ -17,7 +17,9 @@ class AddEditViewController: UIViewController {
     @IBOutlet weak var scGasType: UISegmentedControl!
     @IBOutlet weak var btAddEdit: UIButton!
     @IBOutlet weak var loading: UIActivityIndicatorView!
-
+    
+    var car: Cars!
+    
     // MARK: - Super Methods
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +27,32 @@ class AddEditViewController: UIViewController {
     
     // MARK: - IBActions
     @IBAction func addEdit(_ sender: UIButton) {
+        
+        if car == nil {
+            car = Cars()
+            
+        }
+     
+        if let name = tfName.text{
+            car.name = name
+            
+        }
+        else {print("Nome errado")}
+        car.brand = tfBrand.text!
+        car.price = Double(tfPrice.text ?? "0")!
+        car.gasType = scGasType.selectedSegmentIndex
+        
+        
+        
+        REST.save(car: car) { (sucess) in
+            self.goBack()
+        }
     }
-
+    
+    func goBack(){
+        DispatchQueue.main.async {
+            self.navigationController?.popViewController(animated: true)
+            
+        }
+    }
 }
--- Carangas/REST.swift
+++ Carangas/REST.swift
@@ -1,27 +1,118 @@
-//
-//  REST.swift
-//  Carangas
-//
-//  Created by Rafael Hartmann on 15/03/21.
-//  Copyright © 2021 Eric Brito. All rights reserved.
-//
-
 import Foundation
 
+enum CarError{
+    
+    case url
+    case taskError(error: Error)
+    case noResponse
+    case noData
+    case responseStatusCode(code: Int)
+    case InvalidJSON
+}
 
 class REST {
-     
+    
     private static let basePath = "https://carangas.herokuapp.com/cars"
-    private let configuration: URLSessionConfiguration = {
+    
+    /*
+     basePath é a URL padão da nossa API
+     
+     configuration é a configuração da sessão,
+     a config é o que usamos para usar os metodos de configuração
+     .default é o tipo padrao de configuração
+     
+     allowsCellularAccess é se da pra usar dados moveis para acessar
+     httpAditionalHeaders é o que vem de nome do cabeçalho
+     timeoutIntervalForRequest é o temo maximo de requisiç ao antes de dar erro
+     httpMaximumConnectionsPerHost maximo de conexao, como downloads e coisas assim
+     */
+    
+    private static let configuration: URLSessionConfiguration = {
         let config = URLSessionConfiguration.default
         config.allowsCellularAccess = false
         config.httpAdditionalHeaders = ["Content-Type":"application/json" ]
         config.timeoutIntervalForRequest = 30.0
+        config.httpMaximumConnectionsPerHost = 5
+        return config
     }()
     
     
-    private static let session = URLSession(configuration: )
+    private static let session = URLSession(configuration: configuration )
     
-    
-    
+    class func loadCars(onComplete: @escaping ([Cars]) -> Void, onError: @escaping (CarError) -> Void){
+        
+        guard let url = URL(string: basePath) else {
+            onError(.url)
+            
+            return
+        }
+        let dataTask = session.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+            
+            if error == nil{
+                
+                guard let response = response as? HTTPURLResponse else {
+                    onError(.noResponse)
+                    return}
+                if response.statusCode == 200{
+                    
+                    guard let data = data else {
+                        
+                        return}
+                    do{
+                        let cars = try JSONDecoder().decode([Cars].self, from: data)
+                        onComplete(cars)
+                        
+                        
+                    }catch{
+                        print("Salve primo")
+                        onError(.InvalidJSON)
+                    }
+                    
+                }else{
+                    print("Algum status inválido pelo servidor!!")
+                    onError(.responseStatusCode(code: response.statusCode))
+                }
+            }else{
+                
+                onError(.taskError(error: error!))
+            }
+        }
+        dataTask.resume()
+        
+    }
+    //Metodo post agora. para poder postar um carro novo
+    class func save(car: Cars, onComplete: @escaping (Bool) -> Void){
+        
+        guard let url = URL(string: basePath) else {
+            onComplete(false)
+            
+            return
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        guard let json = try? JSONEncoder().encode(car) else{
+            onComplete(false)
+            return
+            
+        }
+        request.httpBody = json
+        
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            if error == nil{
+                
+                guard let response = response as? HTTPURLResponse, response.statusCode == 200, let _ = data else{
+                    onComplete(false)
+                    return
+                }
+                onComplete(true)
+            }
+            else{
+                
+                onComplete(false)
+                
+            }
+        }
+        dataTask.resume()
+    }
 }
